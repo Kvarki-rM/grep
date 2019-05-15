1cea80a62b4484cf4aee206aed653006417a7053
Kirill <7kreker7@gmail.com>
2019-05-15 16:36:32 +0300

    Initial commit

---
+++ src/test/java/task2/Tester.java
@@ -0,0 +1,10 @@
+package task2;
+
+//import org.junit.jupiter.api.Test;
+//import org.junit.Test;
+//import java.util.Objects;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Tester {
+}
