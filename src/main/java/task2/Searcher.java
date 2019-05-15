95d173457ba6ea4791f92d964066d4c97b50b393
Kirill <7kreker7@gmail.com>
2019-05-15 17:22:40 +0300

    1 ver

--- src/main/java/task2/Searcher.java
+++ src/main/java/task2/Searcher.java
@@ -1,9 +1,11 @@
 package task2;
 
+import java.util.*;
 import java.io.*;
 import java.util.Scanner;
 import java.util.Arrays;
 
+//grep [-v] [-i] [-r] word Example.txt
 public class Searcher {
     public static void main(String[] args) throws Exception {
 
@@ -11,24 +13,25 @@ public class Searcher {
         String command = in.nextLine();
         in.close();
         String[] temp = command.split(" ");
+        String search = temp[temp.length - 2];
+        String sourse = "Input\\" + temp[temp.length - 1];
 
-        String sourse = "C:\\Users\\Kvarki.rM\\IdeaProjects\\Console\\Input\\" + temp[temp.length - 1];
-        // try {
-        //     File file = new File(sourse);
-        //     //создаем объект FileReader для объекта File
-        //     FileReader fr = new FileReader(file);
-        //     //создаем BufferedReader с существующего FileReader для построчного считывания
-        //     BufferedReader reader = new BufferedReader(fr);
-        //     // считаем сначала первую строку
-        //     String line = reader.readLine();
-        //     while (line != null) {
-        //         System.out.println(line);
-        //         // считываем остальные строки в цикле
-        //         line = reader.readLine();
-        //     }
-        // } catch (IOException e) {
-        //     e.printStackTrace();
-        // }
+        //try {
+        //    File file = new File(sourse);
+        //    //создаем объект FileReader для объекта File
+        //    FileReader fr = new FileReader(file);
+        //    //создаем BufferedReader с существующего FileReader для построчного считывания
+        //    BufferedReader reader = new BufferedReader(fr);
+        //    // считаем сначала первую строку
+        //    String line = reader.readLine();
+        //    while (line != null) {
+        //        System.out.println(line);
+        //        // считываем остальные строки в цикле
+        //        line = reader.readLine();
+        //    }
+        //} catch (IOException e) {
+        //    e.printStackTrace();
+        //}
 
     }
 }
