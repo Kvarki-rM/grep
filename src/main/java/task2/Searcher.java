cc5e64f76fe0d3adb9b1aa6b36871dcf2be9deab
Kirill <7kreker7@gmail.com>
2019-05-19 22:28:27 +0300

    1 ver

--- src/main/java/task2/Searcher.java
+++ src/main/java/task2/Searcher.java
@@ -2,19 +2,27 @@ package task2;
 
 import java.io.*;
 import java.util.*;
-import java.util.Scanner;
 
-//                      grep [-v] [-i] [-r] word Example.txt               grep [-v] [-i] по Example.txt
+//                      grep [-v] [-i] [-r] word Example.txt               grep [-v] [-i] по Example.txt  grep [-i] [-r] Старик рыбачил один Example.txt сказал старик.
 
-public class Searcher {
+class Searcher {
+
     public static void main(String[] args) {
+        System.out.println(lookFor("grep [-i] по Example.txt"));
+    }
 
-        Scanner in = new Scanner(System.in);
-        String command = in.nextLine();
-        in.close();
-        String[] temp = command.split(" ");
-        String search = temp[temp.length - 2];
-        String sourse = "Input\\" + temp[temp.length - 1];
+    String input(String val) {
+        if (val == null) {
+            Scanner in = new Scanner(System.in);
+            String command = in.nextLine();
+            in.close();
+            return lookFor(command);
+        } else return lookFor(val);
+    }
+
+    private static String lookFor(String input) {
+
+        String[] temp = input.split(" ");
 
         int leng = 2;
         for (String it : temp) {
@@ -35,14 +43,18 @@ public class Searcher {
             if (temp[i].equals("[-i]")) ignore = true;
         }
 
-        for (int i = 0; i < temp.length - 2; i++) {
-
-        }
-
         //-r (regex) вместо слова задаёт регулярное выражение для поиска (на консоль выводятся только строки, содержащие данное выражение)
         //-v инвертирует условие фильтрации (выводится только то, что ему НЕ соответствует)
         //-i игнорировать регистр слов
 
+        return reader(regex, invert, ignore, temp, leng);
+    }
+
+
+    private static String reader(Boolean regex, Boolean invert, Boolean ignore, String[] value, int leng) {
+        StringBuilder end = new StringBuilder();
+        String search = value[value.length - 2];
+        String sourse = "Input\\" + value[value.length - 1];
         try {
             File file = new File(sourse);
             FileReader fr = new FileReader(file);
@@ -58,16 +70,43 @@ public class Searcher {
             while (line != null) {
                 list.add(false);
                 String[] word = line.split(" ");
-                for (int i = 0; i < word.length; i++)
-                    if (ignore) {
-                        if (word[i].equalsIgnoreCase(search)) {
+                for (int i = 0; i < word.length; i++) {
+                    if (!regex) {
+                        if (ignore) {
+                            if (word[i].equalsIgnoreCase(search)) {
+                                list.set(num, true);
+                                break;
+                            }
+                        } else if (Objects.equals(word[i], search)) {
                             list.set(num, true);
                             break;
                         }
-                    } else if (Objects.equals(word[i], search)) {
-                        list.set(num, true);
-                        break;
+                    } else {
+
+                        int counter = 0;
+                        for (int j = 0; j < value.length - leng; j++) {
+                            if (word.length <= i + value.length - leng) {
+                                break;
+                            }
+                            int i1 = value.length - leng + j;
+                            if (ignore) {
+                                if (word[i + j].equalsIgnoreCase(value[i1])) {
+                                    counter++;
+                                    if (counter == value.length - leng) {
+                                        list.set(num, true);
+                                        break;
+                                    }
+                                } else j = value.length - leng;
+                            } else if (Objects.equals(word[i + j], value[i1])) {
+                                counter++;
+                                if (counter == value.length - leng) {
+                                    list.set(num, true);
+                                    break;
+                                }
+                            } else j = value.length - leng;
+                        }
                     }
+                }
                 num++;
                 line = reader.readLine();
 
@@ -77,13 +116,21 @@ public class Searcher {
 
             while (line2 != null) {
                 if (!invert) {
-                    if (list.get(num)) System.out.println(line2);
-                } else if (!list.get(num)) System.out.println(line2);
+                    if (list.get(num)) {
+                        end.append(line2);
+
+                    }
+                } else if (!list.get(num)) {
+                    end.append(line2);
+                }
                 line2 = reader2.readLine();
                 num++;
             }
-        } catch (IOException e) {
+        } catch (
+                IOException e) {
             e.printStackTrace();
         }
+        return end.toString();
+
     }
 }
